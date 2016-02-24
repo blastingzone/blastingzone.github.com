ecdca902e2b2cc6e7cbd81e4c6e9b0906ea5e180
MoonJS <blastingzone@naver.com>
2016-02-25 02:52:24 +0900

    keyboard keycode data file added

---
+++ webGame/js/constKeyboardEventCode.js
@@ -0,0 +1,13 @@
+/**
+ * Created by SmartAndWise on 2016-02-25.
+ */
+
+const ARROW_KEY_UP = 38;
+const ARROW_KEY_DOWN = 40;
+const ARROW_KEY_LEFT = 37;
+const ARROW_KEY_RIGHT = 39;
+
+const A_KEY = 65;
+const S_KEY = 83;
+const D_KEY = 68;
+const F_KEY = 70;
