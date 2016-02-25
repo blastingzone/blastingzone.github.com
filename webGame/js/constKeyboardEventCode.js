c90f9bae5384e83a5a72389e037a2d5f33dbc742
MoonJS <blastingzone@naver.com>
2016-02-26 01:03:18 +0900

    make key code

--- webGame/js/constKeyboardEventCode.js
+++ webGame/js/constKeyboardEventCode.js
@@ -7,6 +7,14 @@ const ARROW_KEY_DOWN = 40;
 const ARROW_KEY_LEFT = 37;
 const ARROW_KEY_RIGHT = 39;
 
+const SPACE_KEY = 32;
+const ENTER_KEY = 13;
+
+const Q_KEY = 81;
+const W_KEY = 87;
+const E_KEY = 69;
+const R_KEY = 82;
+
 const A_KEY = 65;
 const S_KEY = 83;
 const D_KEY = 68;
